8bf1c4e73eed81d68b5ddde1ef81e59d057aa5e7
Dubovec-Vladislav <dubovecvladislav0@gmail.com>
2023-12-30 19:53:28 +0300

    Deliting .prettierrc fron rep

--- data-output.js
+++ data-output.js
@@ -1,27 +1,43 @@
 // Получаем элемент, в который будем вставлять данные
-const outputElement = document.getElementById('output');
+const outputElement = document.querySelector('.wrapper');
 
 // Создаем HTML-строку на основе данных
 let distance = 0;
-let htmlString = '<ul class="list">';
+let htmlString = '<div class="main__block">';
+htmlString += '<ul class="cars__list">';
 cars.forEach((item) => {
   distance += item.Пробег;
-  htmlString += '<li class="item">';
-  htmlString += `<div class="title">Название: <span>${item['Название']}</span></div>`;
-  htmlString += `<div class="time">Время работы на маршруте: <span>${(8 - item['ОставшеесяВремяРаботы']).toFixed(2)}</span> час(-а)</div>`;
-  htmlString += `<div class="distance">Пробег: <span>${item['Пробег']}</span> км</div>`;
+  htmlString += '<li class="cars__list-item">';
+  htmlString += `<div class="cars__list-title"><span>Название:</span></span> ${item['Название']}</div>`;
+  htmlString += `<div class="cars__list-point"><span>Время работы на маршруте:</span> ${(8 - item['ОставшеесяВремяРаботы']).toFixed(2)} час(-а)</div>`;
+  htmlString += `<div class="cars__list-point"><span>Пробег:</span> ${item['Пробег']} км</div>`;
 
-  // Форматируем данные о маршруте
   const routeString = Object.entries(item['Маршрут'])
-    .map(([point, distance]) => `${point} - ${distance} поездок(-ки)`)
-    .join(', ');
-
-  htmlString += `<div class="route">Маршрут: <span>${routeString}</span></div>`;
-  htmlString += '</li>';
+    .map(([point, distance]) => `<li class="cars__list-route-item">${point} - ${distance} поездок(-ки)</li>`)
+    .join('');
+  htmlString += `<div class="cars__list-point"><span>Маршрут:</span> <ul class="cars__list-route">${routeString}</ul></div>`;
 });
 htmlString += '</ul>';
-htmlString += `<div class="total-distance">Общий пробег: <span>${distance}</span></div>`;
-htmlString += `<div class="total-distance">% Доставки: <span>${percent.toFixed(2)}%</span></div>`;
+
+htmlString += '<ul class="points__list">';
+cars.forEach((item) => {
+  distance += item.Пробег;
+  htmlString += '<li class="cars__list-item">';
+  htmlString += `<div class="cars__list-title"><span>Название:</span></span> ${item['Название']}</div>`;
+  htmlString += `<div class="cars__list-point"><span>Время работы на маршруте:</span> ${(8 - item['ОставшеесяВремяРаботы']).toFixed(2)} час(-а)</div>`;
+  htmlString += `<div class="cars__list-point"><span>Пробег:</span> ${item['Пробег']} км</div>`;
+
+  const routeString = Object.entries(item['Маршрут'])
+    .map(([point, distance]) => `<li class="cars__list-route-item">${point} - ${distance} поездок(-ки)</li>`)
+    .join('');
+  htmlString += `<div class="cars__list-point"><span>Маршрут:</span> <ul class="cars__list-route">${routeString}</ul></div>`;
+});
+htmlString += '</ul>';
+
+htmlString += '</div>'
+
+htmlString += `<div class="total__inf">Общий пробег: <span>${distance}</span></div>`;
+htmlString += `<div class="total__inf">% Доставки: <span>${percent.toFixed(2)}%</span></div>`;
 
 // Вставляем HTML в элемент
 outputElement.innerHTML = htmlString;
