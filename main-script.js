2c55a00c8a2ed7193e0dc776e507e105d69e5dd2
Dubovec-Vladislav <dubovecvladislav0@gmail.com>
2024-01-09 11:34:49 +0300

    Creating data input

--- main-script.js
+++ main-script.js
@@ -1,70 +1,89 @@
 let NUMBER_OF_CARS = 3;
 
-// ---------- Информация о точках ---------- //
-const depotToCareerDistance = 6; // Расстояние от депо до карьера
-const depotToPointsDistances = [10, 8, 13]; // Расстояния от депо до каждой точки
-const careerToPointsDistances = [18, 12, 7]; // Расстояния от карьера до каждой точки
-const needs = [30, 40, 50]; // Потребности для каждой точки
+function calculateData() {
+  const pointCount = document.getElementById('pointCount').value;
+  NUMBER_OF_CARS = parseInt(document.getElementById('carCount').value);
 
-const pointsInfo = Array.from({ length: depotToPointsDistances.length }, (_, index) => ({
-  name: `Пункт ${index + 1}`,
-  depotTo: depotToPointsDistances[index],
-  careerTo: careerToPointsDistances[index],
-  need: needs[index],
-}));
-// ----------------------------------------- //
+  const depotToPointsDistances = [];
+  const careerToPointsDistances = [];
+  const needs = [];
 
-// -------- Транспортные характеристики ------ //
-const transport = {
-  speed: 40,
-  liftingCapacity: 6,
-  capacityUtilizationFactor: 0.9,
-  loadingAndUnloadingTime: 0.33,
-};
+  for (let i = 0; i < pointCount; i++) {
+    const depotDistance = parseFloat(document.getElementById(`depotDistance${i}`).value);
+    const careerDistance = parseFloat(document.getElementById(`careerDistance${i}`).value);
+    const pointNeeds = parseFloat(document.getElementById(`needs${i}`).value);
+    depotToPointsDistances.push(depotDistance);
+    careerToPointsDistances.push(careerDistance);
+    needs.push(pointNeeds);
+  }
 
-const actualLiftingCapacity = transport.liftingCapacity * transport.capacityUtilizationFactor;
-// ----------------------------------------- //
+  const transport = {
+    speed: parseFloat(document.getElementById('speed').value),
+    liftingCapacity: parseFloat(document.getElementById('liftingCapacity').value),
+    capacityUtilizationFactor: parseFloat(document.getElementById('capacityUtilizationFactor').value),
+    loadingAndUnloadingTime: parseFloat(document.getElementById('loadingAndUnloadingTime').value),
+  };
 
-// ------- 1. Определение количество ездок в каждую точку ------ //
-const trips = pointsInfo.map((point) => Math.ceil(point.need / actualLiftingCapacity));
-// ------------------------------------------------------------- //
+  main(depotToPointsDistances, careerToPointsDistances, needs, transport);
+}
 
-// ------------- 2. Определение очередности объезда ------------ //
-const queueList = getSortedQueueList(pointsInfo); // -> { Point3: 1, Point2: 2, Point1: 3 }
+function main(depotToPointsDistances, careerToPointsDistances, needs, transport) {
+  // -------- Транспортные характеристики ------ //
+  const actualLiftingCapacity = transport.liftingCapacity * transport.capacityUtilizationFactor;
+  // ----------------------------------------- //
 
-// Формирование информации о точках
-const points = pointsInfo.map((point, index) => ({
-  Название: point.name,
-  РасстояниеОтДепоДоЭтойТочки: point.depotTo,
-  РасстояниеОтКарьераДоЭтойТочки: point.careerTo,
-  КоличествоЕздок: trips[index],
-  ОчередностьОбъезда: queueList[`Point${index + 1}`],
-  Потребность: needs[index],
-  ОставшаясяПотребность: point.need,
-}));
+  // ---------- Информация о точках ---------- //
+  const pointsInfo = Array.from({ length: depotToPointsDistances.length }, (_, index) => ({
+    name: `Пункт ${index + 1}`,
+    depotTo: depotToPointsDistances[index],
+    careerTo: careerToPointsDistances[index],
+    need: needs[index],
+  }));
+  // ----------------------------------------- //
 
-// Сортировка точек по порядку объезда
-points.sort((a, b) => a['ОчередностьОбъезда'] - b['ОчередностьОбъезда']); // 1 -> 2 -> 3 -> ...
-// ------------------------------------------------------------- //
+  // ------- 1. Определение количество ездок в каждую точку ------ //
+  const trips = pointsInfo.map((point) => Math.ceil(point.need / actualLiftingCapacity));
+  // ------------------------------------------------------------- //
 
-// --- 3. Планирование последних поездок и возвращения в депо -- //
+  // ------------- 2. Определение очередности объезда ------------ //
+  const queueList = getSortedQueueList(pointsInfo); // -> { Point3: 1, Point2: 2, Point1: 3 }
 
-// Информация о машинах
-const cars = Array.from({ length: NUMBER_OF_CARS }, (_, index) => ({
-  'Название': `Машина ${index + 1}`,
-  'ОставшеесяВремяРаботы': 8,
-  'Пробег': 0,
-  'Маршрут': {},
-}));
+  // Формирование информации о точках
+  const points = pointsInfo.map((point, index) => ({
+    Название: point.name,
+    РасстояниеОтДепоДоЭтойТочки: point.depotTo,
+    РасстояниеОтКарьераДоЭтойТочки: point.careerTo,
+    КоличествоЕздок: trips[index],
+    ОчередностьОбъезда: queueList[`Point${index + 1}`],
+    Потребность: needs[index],
+    ОставшаясяПотребность: point.need,
+  }));
 
-// Планирование последних поездок и возвращения в депо
-planLastTripsAndReturn(cars, points);
+  // Сортировка точек по порядку объезда
+  points.sort((a, b) => a['ОчередностьОбъезда'] - b['ОчередностьОбъезда']); // 1 -> 2 -> 3 -> ...
+  // ------------------------------------------------------------- //
 
-// Планирование других маршрутов для автомобилей
-planOtherRoutes(cars, points);
+  // --- 3. Планирование последних поездок и возвращения в депо -- //
 
-swapFirstRouteKeyValue(cars);
+  // Информация о машинах
+  const cars = Array.from({ length: NUMBER_OF_CARS }, (_, index) => ({
+    'Название': `Машина ${index + 1}`,
+    'ОставшеесяВремяРаботы': 8,
+    'Пробег': 0,
+    'Маршрут': {},
+  }));
 
-// Планирование других маршрутов для автомобилей
-const percent = calculateCompletionPercentage(points, pointsInfo);
-// ------------------------------------------------------------- //
+  // Планирование последних поездок и возвращения в депо
+  planLastTripsAndReturn(cars, points, transport, actualLiftingCapacity);
+
+  // Планирование других маршрутов для автомобилей
+  planOtherRoutes(cars, points, transport, actualLiftingCapacity);
+
+  swapFirstRouteKeyValue(cars);
+
+  // Планирование других маршрутов для автомобилей
+  const percent = calculateCompletionPercentage(points, pointsInfo);
+
+  dataOutput(cars, points, percent);
+  // ------------------------------------------------------------- //
+}
--- utils.js
+++ utils.js
@@ -1,3 +1,30 @@
+function createPointInputs() {
+  const pointCount = document.getElementById('pointCount').value;
+
+  let inputsHtml = '';
+
+  for (let i = 0; i < pointCount; i++) {
+    inputsHtml += `
+      <h3>Точка ${i + 1}</h3>
+      <label>Расстояние от депо:</label>
+      <input type="number" id="depotDistance${i}" min="1" value="10">
+
+      <label>Расстояние от карьера:</label>
+      <input type="number" id="careerDistance${i}" min="1" value="10">
+
+      <label>Потребность в тоннах груза:</label>
+      <input type="number" id="needs${i}" min="1" value="30">
+    `;
+  }
+
+  document.getElementById('pointInputs').innerHTML = inputsHtml;
+
+  // const depotToCareerDistance = 6; // Расстояние от депо до карьера
+  // const depotToPointsDistances = [10, 8, 13]; // Расстояния от депо до каждой точки
+  // const careerToPointsDistances = [18, 12, 7]; // Расстояния от карьера до каждой точки
+  // const needs = [30, 40, 50]; // Потребности для каждой точки
+}
+
 // Вспомогательная функция для получения отсортированного списка очередности объезда
 function getSortedQueueList(pointsInfo) {
   return Object.fromEntries(
@@ -11,20 +38,22 @@ function getSortedQueueList(pointsInfo) {
 // Вспомогательная для смены первого и последнего пункта маршрута местами
 function swapFirstRouteKeyValue(cars) {
   cars.forEach((car) => {
-    const route = car.Маршрут;
-    const keys = Object.keys(route);
-    const values = Object.values(route);
+    if (Object.keys(car.Маршрут).length !== 0) {
+      const route = car.Маршрут;
+      const keys = Object.keys(route);
+      const values = Object.values(route);
 
-    // Меняем местами первый и последний ключ
-    [keys[0], keys[keys.length - 1]] = [keys[keys.length - 1], keys[0]];
-    // Меняем местами первое и последнее значение
-    [values[0], values[values.length - 1]] = [values[values.length - 1], values[0]];
+      // Меняем местами первый и последний ключ
+      [keys[0], keys[keys.length - 1]] = [keys[keys.length - 1], keys[0]];
+      // Меняем местами первое и последнее значение
+      [values[0], values[values.length - 1]] = [values[values.length - 1], values[0]];
 
-    // Обновляем поле "Маршрут" в объекте
-    car.Маршрут = keys.reduce((acc, key, index) => {
-      acc[key] = values[index];
-      return acc;
-    }, {});
+      // Обновляем поле "Маршрут" в объекте
+      car.Маршрут = keys.reduce((acc, key, index) => {
+        acc[key] = values[index];
+        return acc;
+      }, {});
+    } // Проверяем есть ли что-то в маршруте
   });
 }
 
@@ -62,26 +91,26 @@ function updateCarData(cars, lastPoint, time, mileage, route) {
 }
 
 // Функция для обновления данных по последней точке
-function updateLastPointData(lastPoint, numberOfTrips) {
-  lastPoint.КоличествоЕздок -= numberOfTrips * cars.length;
+function updateLastPointData(lastPoint, numberOfTrips, carsLength, actualLiftingCapacity) {
+  lastPoint.КоличествоЕздок -= numberOfTrips * carsLength;
   if (lastPoint.КоличествоЕздок < 0) lastPoint.КоличествоЕздок = 0;
-  lastPoint.ОставшаясяПотребность -= numberOfTrips * cars.length * actualLiftingCapacity;
+  lastPoint.ОставшаясяПотребность -= numberOfTrips * carsLength * actualLiftingCapacity;
   if (lastPoint.ОставшаясяПотребность < 0) lastPoint.ОставшаясяПотребность = 0;
 }
 
 // Главная функция для планирование последних поездок и возвращения в депо
-function planLastTripsAndReturn(cars, points) {
+function planLastTripsAndReturn(cars, points, transport, actualLiftingCapacity) {
   const lastPoint = points[points.length - 1];
   const numberOfTripsToLastPoint = NUMBER_OF_CARS === points.length ? Math.floor(lastPoint.КоличествоЕздок / cars.length) || 1 : 1;
-
+  
   const { time, mileage, route } = calculateTimeAndMileageToLastPoint(lastPoint, transport, numberOfTripsToLastPoint);
   updateCarData(cars, lastPoint, time, mileage, route);
-  updateLastPointData(lastPoint, numberOfTripsToLastPoint);
+  updateLastPointData(lastPoint, numberOfTripsToLastPoint, cars.length, actualLiftingCapacity);
 }
 // ------------------------------------------------------------- //
 
 // ------- Планирование других маршрутов для автомобилей ------- //
-function planOtherRoutes(cars, points) {
+function planOtherRoutes(cars, points, transport, actualLiftingCapacity) {
   for (const point of points) {
     for (const car of cars) {
       let tripCounter = car.Маршрут[point.Название] ? car.Маршрут[point.Название] : 0; // Если у нас уже были ездки на этой машине в этот пункт,
